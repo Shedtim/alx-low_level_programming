42bf877f1d20ab6ef3f982112b2e09384524121a
Timothy Shedrack <shed_tim@yahoo.com>
2022-09-21 18:00:42 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,13 +2,14 @@
 #include <stdio.h>
 
 /**
- * main - check the code for ALX School students.
- * 
- * Return: Always 0.
- */
-
+* main - check the code for ALX School students.
+*
+* Return: Always 0.
+*/
 int main(void)
+
 {
+
 char s1[98] = "Hello ";
 char s2[] = "World!\n";
 char *ptr;
@@ -20,4 +21,5 @@ printf("%s", s1);
 printf("%s", s2);
 printf("%s", ptr);
 return (0);
+
 }
