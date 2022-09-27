1edbf21d8578cd23306b05611cd205325b4f002a
Timothy Shedrack <shed_tim@yahoo.com>
2022-09-26 20:41:10 -0700

    3-strspn.c

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -3,13 +3,13 @@
 /**
  * _strspn - get length of prefix substring
  *
- * Description: return length of bytes of string which consist                                  
+ * Description: return length of bytes of string which consist
  * entirely of bytes in a specific set of characters
  *
  * @str: string to check
  * @accept: array of bytes acceptable
  *
- * Return: the number of bytes in the initial segment of @str which                             
+ * Return: the number of bytes in the initial segment of @str which
  * consist only bytes from @accept
  */
 unsigned int _strspn(char *str, char *accept)
