5ffa4fa4feb01386c1d4b79633e27c2c428d8f70
Timothy Shedrack <shed_tim@yahoo.com>
2022-09-30 17:47:19 -0700

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -3,7 +3,11 @@
 
 /**
  * main - function
- * @argc: length of argv                                                                       * @argv: number of argument                                                                   * Return: Always 0                                                                            */
+ * @argc: length of argv
+ * @argv: number of argument
+ * Return: Always 0
+ */
+
 int main(int argc, char *argv[])
 {
 	/*Declaring variables*/
